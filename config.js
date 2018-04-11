93caac4ce0bb1ca0c84ef095514d0d141e518576
root <root@localhost.localdomain>
2018-04-11 18:08:38 -0500

    v1.0https

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
     URLbroker: "mqtt://148.220.52.100",
-    // URLbroker: "mqtt://148.220.63.181",
-    URLportalRFID : "http://localhost/portalRFID/api/",
+    URLportalRFID : "https://portalinformatica.uaq.mx/portalRFID/Api/",
     error: 2,
     correcto: 1
 };
